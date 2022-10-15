7231f86572920d8fecf56ed7169d9be99a6781e4
RaphaelSch <raphael.scharff@gmail.com>
2022-10-15 15:29:04 +0200

    Adjust to new DB format

--- core/tanks.js
+++ core/tanks.js
@@ -5,189 +5,189 @@ const tierMax = 10;
 
 var exclude_tanks = [];
 
-async function query_tier(tier){
+async function query_tier(tier) {
     var res = await db.query("SELECT GROUP_CONCAT(name SEPARATOR ', ') as value FROM tanks WHERE tanks.tier='" + tier + "' ORDER BY name ASC");
 
     try {
-        if(res){
+        if (res) {
             return res[0].value;
         }
         return;
     }
-    catch (err){
+    catch (err) {
         console.error(err);
         return null;
     }
 }
 
-async function query_name(name){
-    var res = await db.query("SELECT name, mark, max_dmg, note FROM tanks WHERE tanks.key='" + name + "'");
+async function query_name(name) {
+    var res = await db.query("SELECT name, mark, max_dmg, note FROM tanks WHERE tanks.trigger_word='" + name + "'");
 
     try {
-        if(res[0]){
+        if (res[0]) {
             tank = res[0];
             return tank.name + ' : ' + tank.mark + ' Marque(s) - Dégats max : ' + tank.max_dmg;
         }
         return;
     }
-    catch (err){
+    catch (err) {
         console.error(err);
         return;
     }
 }
 
-async function query_nation(nation){
+async function query_nation(nation) {
     var res = await db.query("SELECT GROUP_CONCAT(name SEPARATOR ', ') as value FROM tanks WHERE tanks.nation='" + nation + "' ORDER BY name ASC");
 
     try {
-        if(res[0]){
+        if (res[0]) {
             return res[0].value;
         }
         return;
     }
-    catch (err){
+    catch (err) {
         console.error(err);
         return;
     }
 }
 
-async function query_type(type){
+async function query_type(type) {
     var res = await db.query("SELECT GROUP_CONCAT(name SEPARATOR ', ') as value FROM tanks WHERE tanks.type='" + type + "' ORDER BY name ASC");
 
     try {
-        if(res[0]){
+        if (res[0]) {
             return res[0].value;
         }
         return;
     }
-    catch (err){
+    catch (err) {
         console.error(err);
         return;
     }
 }
 
-async function query_random(){
+async function query_random() {
     var count = 0;
 
     // Get number of Tier 10
-    var res = await db.query("SELECT COUNT(`key`) as count FROM tanks WHERE tier = 10");
+    var res = await db.query("SELECT COUNT(`id`) as count FROM tanks WHERE tier = 10");
     try {
-        if(res[0]){
+        if (res[0]) {
             count = res[0].count;
         }
     }
-    catch (err){
+    catch (err) {
         console.error(err);
         return;
     }
 
     // Empty array
-    if(exclude_tanks.length == count) exclude_tanks = [];
-    
+    if (exclude_tanks.length == count) exclude_tanks = [];
+
     // Exclude tank
     var sql_exclude = "";
-    exclude_tanks.forEach(tank => sql_exclude += " AND `key` != '" + tank + "'");
-    
+    exclude_tanks.forEach(tank => sql_exclude += " AND `id` != '" + tank + "'");
+
     // Random
-    var res = await db.query("SELECT `key`, `name` FROM tanks WHERE tier = 10 " + sql_exclude + " ORDER BY RAND() LIMIT 1");
+    var res = await db.query("SELECT `id`, `name` FROM tanks WHERE tier = 10 " + sql_exclude + " ORDER BY RAND() LIMIT 1");
     try {
-        if(res[0]){
-            exclude_tanks.push(res[0].key);
+        if (res[0]) {
+            exclude_tanks.push(res[0].id);
             return res[0].name;
         }
         return;
     }
-    catch (err){
+    catch (err) {
         console.error(err);
         return;
     }
 }
 
-async function get_alias_name(request){
+async function get_alias_name(request) {
     var res = await db.query("SELECT tank FROM alias_tanks WHERE alias_tanks.alias='" + request + "'");
 
     try {
-        if(res[0]){
+        if (res[0]) {
             return res[0].tank;
         }
         return;
     }
-    catch (err){
+    catch (err) {
         console.error(err);
         return null;
     }
 }
 
-async function get_alias_nation(request){
+async function get_alias_nation(request) {
     var res = await db.query("SELECT nation FROM alias_nation WHERE alias_nation.alias='" + request + "'");
 
     try {
-        if(res[0]){
+        if (res[0]) {
             return res[0].nation;
         }
         return;
     }
-    catch (err){
+    catch (err) {
         console.error(err);
         return null;
     }
 }
 
-async function hangar(request){
+async function hangar(request) {
     var result = null;
 
     // Param is not defined or out of range
-    if(!request){
+    if (!request) {
         return "Ecrit \"!char 5\" pour les chars de tier 5 ou \"!char e100\" pour les détails du E100, \"!char fr\" pour les chars Français, ...";
     }
 
     // Param is out of range
-	if(request < tierMin || request > tierMax){
-		return "Le tier que tu m'as demandé est trop bas ou trop haut (entre " + tierMin + " et " + tierMax + ")";
+    if (request < tierMin || request > tierMax) {
+        return "Le tier que tu m'as demandé est trop bas ou trop haut (entre " + tierMin + " et " + tierMax + ")";
     }
 
     // request = random
-    if(request == "random"){
+    if (request == "random") {
         return "Voici un char : " + await query_random();
     }
-    
+
     // request is a tier
-	var param_tier = parseInt(request);
-	if(param_tier){
-		result = await query_tier(param_tier);
-        
-        if(result){
+    var param_tier = parseInt(request);
+    if (param_tier) {
+        result = await query_tier(param_tier);
+
+        if (result) {
             return "Tier " + param_tier + " : " + result;
         }
     }
-    
-	// request is a Nation
-	// Alias
-	var nation = await get_alias_nation(request);
-	if(nation){
-		request = nation;
-	}
+
+    // request is a Nation
+    // Alias
+    var nation = await get_alias_nation(request);
+    if (nation) {
+        request = nation;
+    }
 
     result = await query_nation(request);
-	if (result){
-		return "Char(s) " + request + " : " + result;
-	}
-    
+    if (result) {
+        return "Char(s) " + request + " : " + result;
+    }
+
     // request is a tank
-	// Alias
-	var tank = await get_alias_name(request);
-	if(tank){
+    // Alias
+    var tank = await get_alias_name(request);
+    if (tank) {
         request = tank
     }
 
     result = await query_name(request);
-    if (result){
+    if (result) {
         return result;
     }
 
     // request is a type
     result = await query_type(request);
-    if (result){
+    if (result) {
         return "Char(s) " + request + " : " + result;
     }
 
@@ -195,4 +195,4 @@ async function hangar(request){
     return "Aucun résultat :(";
 }
 
-module.exports = {hangar};
+module.exports = { hangar };
--- www/tanks.php
+++ www/tanks.php
@@ -4,7 +4,7 @@ require_once('src/php/header.php');
 //POST
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($_POST['action'] == "add" && !empty($_POST['form_key']) && !empty($_POST['form_nation']) && !empty($_POST['form_tier']) && !empty($_POST['form_type']) && !empty($_POST['form_name'])) {
-        $key = strtolower(sanitise_input($db, $_POST['form_key']));
+        $trigger_word = strtolower(sanitise_input($db, $_POST['form_key']));
         $nation = sanitise_input($db, $_POST['form_nation']);
         $tier = sanitise_input($db, $_POST['form_tier']);
         $name = sanitise_input($db, $_POST['form_name']);
@@ -15,21 +15,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         $max_dmg = empty($max_dmg) ? 0 : $max_dmg;
 
-        db_query_no_result($db, "INSERT INTO tanks VALUES ('$key', '$nation', '$tier', '$name', '$mark', '$max_dmg', '$note', '$type')");
+        db_query_no_result($db, "INSERT INTO tanks VALUES (NULL, '$trigger_word', '$nation', '$tier', '$name', '$mark', '$max_dmg', '$note', '$type')");
     }
 
     if ($_POST['action'] == "del") {
-        $key = sanitise_input($db, $_POST['key']);
-        db_query_no_result($db, "DELETE FROM tanks WHERE tanks.key='$key'");
+        $id = sanitise_input($db, $_POST['id']);
+        db_query_no_result($db, "DELETE FROM tanks WHERE tanks.id='$id'");
     }
 
     if ($_POST['action'] == "edit") {
-        $key = sanitise_input($db, $_POST['swal-key']);
+        $id = sanitise_input($db, $_POST['swal-key']);
+        $trigger_word = sanitise_input($db, $_POST['swal-trigger']);
         $name = sanitise_input($db, $_POST['swal-name']);
         $dmg = sanitise_input($db, $_POST['swal-dmg']);
         $mark = sanitise_input($db, $_POST['swal-mark']);
         $note = sanitise_input($db, $_POST['swal-note']);
-        db_query_no_result($db, "UPDATE `tanks` SET `name` = '$name', `mark` = '$mark', `max_dmg` = '$dmg', `note` = '$note' WHERE `key` = '$key'");
+        $id = sanitise_input($db, $_POST['swal-key']);
+        db_query_no_result($db, "UPDATE `tanks` SET `trigger_word` = '$trigger_word', `name` = '$name', `mark` = '$mark', `max_dmg` = '$dmg', `note` = '$note' WHERE `id` = '$id'");
     }
 
     header('Location: tanks.php');
@@ -38,11 +40,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 // List tank
 $HTML = "";
-$result = db_query_raw($db, "SELECT * FROM tanks ORDER BY `key` ASC");
+$result = db_query_raw($db, "SELECT * FROM tanks ORDER BY `trigger_word` ASC");
 while ($row = mysqli_fetch_assoc($result)) {
     $HTML .= "
-  <tr id='" . $row["key"] . "'>
-    <td id='key'>" . $row["key"] . "</td>
+  <tr id='" . $row["id"] . "'>
+    <td id='trigger_word'>" . $row["trigger_word"] . "</td>
     <td id='name'>" . $row["name"] . "</td>
     <td id='nation'>" . $row["nation"] . "</td>
     <td id='tier'>" . $row["tier"] . "</td>
@@ -52,15 +54,15 @@ while ($row = mysqli_fetch_assoc($result)) {
     <td id='note'>" . $row["note"] . "</td>
     <td>
       <span class='pull-right'>
-        <button onClick='edit_entry(\"" . $row["key"] . "\")' class='btn btn-warning' type='button'><i class='glyphicon glyphicon-pencil'></i></button>
-        <button type='button' class='btn btn-danger' onclick='del_entry(\"" . $row['key'] . "\")'><i class='glyphicon glyphicon-remove'></i></button>
+        <button onClick='edit_entry(\"" . $row["id"] . "\")' class='btn btn-warning' type='button'><i class='glyphicon glyphicon-pencil'></i></button>
+        <button type='button' class='btn btn-danger' onclick='del_entry(\"" . $row['id'] . "\")'><i class='glyphicon glyphicon-remove'></i></button>
       </span>
     </td>
   </tr>";
 }
 
 // Count tank
-$count = db_query($db, "SELECT COUNT(`key`) as value FROM tanks")['value'];
+$count = db_query($db, "SELECT COUNT(`id`) as value FROM tanks")['value'];
 
 ?>
 
@@ -91,7 +93,7 @@ $count = db_query($db, "SELECT COUNT(`key`) as value FROM tanks")['value'];
         <table class="table table-hover table-condensed">
             <thead>
                 <tr>
-                    <th class="col-xs-1">Key</th>
+                    <th class="col-xs-1">Trigger</th>
                     <th class="col-xs-2">Name</th>
                     <th class="col-xs-1">Nation</th>
                     <th class="col-xs-1">Tier</th>
@@ -123,7 +125,7 @@ $count = db_query($db, "SELECT COUNT(`key`) as value FROM tanks")['value'];
                 html: "<form id='swal-form' method='post'>" +
                     "<input type='hidden' name='action' value='add'>" +
                     // Key
-                    "<label>Tank key</label><input type='text' class='form-control' name='form_key' required></br>" +
+                    "<label>Tank trigger</label><input type='text' class='form-control' name='form_key' required></br>" +
                     // Name
                     "<label>Tank name</label><input type='text' class='form-control' name='form_name' required></br>" +
                     // Nation
@@ -153,9 +155,9 @@ $count = db_query($db, "SELECT COUNT(`key`) as value FROM tanks")['value'];
             });
         }
 
-        function del_entry(key) {
-            Swal({
-                title: "Delete '" + key + "' ?",
+        function del_entry(id) {
+            Swal.fire({
+                title: "Delete '" + id + "' ?",
                 type: 'info',
                 showCancelButton: true,
                 confirmButtonColor: '#3085d6',
@@ -166,7 +168,7 @@ $count = db_query($db, "SELECT COUNT(`key`) as value FROM tanks")['value'];
                 if (result.value) {
                     $.post("tanks.php", {
                         action: "del",
-                        key: key
+                        id: id
                     }, function(data) {
                         document.location.reload();
                     });
@@ -174,19 +176,21 @@ $count = db_query($db, "SELECT COUNT(`key`) as value FROM tanks")['value'];
             })
         }
 
-        function edit_entry(key) {
-            var tank = document.getElementById(key).children;
+        function edit_entry(id) {
+            var tank = document.getElementById(id).children;
+            var trigger_word = tank.trigger_word.textContent;
             var name = tank.name.textContent;
             var mark = tank.mark.textContent;
             var max_dmg = tank.max_dmg.textContent;
             var note = tank.note.textContent;
 
-            Swal({
-                title: 'Editing : "' + key + '"',
+            Swal.fire({
+                title: 'Editing : "' + trigger_word + '"',
                 type: 'info',
                 html: "<form id='swal-form' method='post' class='form-vertical'>" +
                     "<input type='hidden' name='action' value='edit'>" +
-                    "<input type='hidden' name='swal-key' value='" + key + "'>" +
+                    "<input type='hidden' name='swal-key' value='" + id + "'>" +
+                    "<label>Trigger</label><input class='form-control' type='text' name='swal-trigger' value='" + trigger_word + "'></br>" +
                     "<label>Tank name</label><input class='form-control' type='text' name='swal-name' value='" + name + "'></br>" +
                     "<label>Mark</label><input class='form-control' type='number' min=0 max=3 name='swal-mark' value='" + mark + "'></br>" +
                     "<label>Damage max</label><input class='form-control' type='number' name='swal-dmg' value='" + max_dmg + "'></br>" +
