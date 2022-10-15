ac8c7a45634ab0b28ef966fb8dea42b3ca74dcec
RaphaelSch <raphael.scharff@gmail.com>
2022-10-15 15:11:39 +0200

    Adjust to new DB format

--- core/reaction.js
+++ core/reaction.js
@@ -4,62 +4,62 @@ const tools = require('./tools.js');
 var exclude_reactions = [];
 var socket = null;
 
-function init(config_init, socket_init){
-	socket = socket_init;
+function init(config_init, socket_init) {
+    socket = socket_init;
 }
 
-async function query_reaction(words){
-	var sql = "( 0";
+async function query_reaction(words) {
+    var sql = "( 0";
 
-	for(var word of words) {
-		sql += " OR reactions.key='" + word + "'";
-	}	
+    for (var word of words) {
+        sql += " OR reactions.trigger_word='" + word + "'";
+    }
 
-	sql += ")"
+    sql += ")"
 
-	// Exclude
-	var sql_exclude = "";
-	exclude_reactions.forEach(reaction => sql_exclude += " AND reactions.key != '" + reaction + "'");
+    // Exclude
+    var sql_exclude = "";
+    exclude_reactions.forEach(reaction => sql_exclude += " AND reactions.trigger_word != '" + reaction + "'");
 
-	var res = await db.query("SELECT `key`, reaction, frequency, timeout FROM reactions WHERE " + sql + sql_exclude + " ORDER BY RAND() LIMIT 1");
+    var res = await db.query("SELECT id, trigger_word, reaction, frequency, timeout FROM reactions WHERE " + sql + sql_exclude + " ORDER BY RAND() LIMIT 1");
     try {
-        if(res[0]){
+        if (res[0]) {
             return res[0];
         }
         return;
     }
-    catch (err){
+    catch (err) {
         console.error(err);
         return null;
     }
 }
 
-async function run(user, message){
-	var words = message.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/['"]+/g, ' ').split(" ");
-	var result = await query_reaction(words);
+async function run(user, message) {
+    var words = message.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/['"]+/g, ' ').split(" ");
+    var result = await query_reaction(words);
 
-	try {
-        if(result){
-			if(tools.getRandomInt(100) <= result.frequency){
-				if(result.timeout > 0){
-					exclude_reactions.push(result.key);
-					
-					setTimeout(function() {
-						exclude_reactions.splice(exclude_reactions.indexOf(result.key), 1);
-						socket.log("[REACTION] '" + result.key + "' has been removed from the exclusion list");
-					}, result.timeout * 1000);
+    try {
+        if (result) {
+            if (tools.getRandomInt(100) <= result.frequency) {
+                if (result.timeout > 0) {
+                    exclude_reactions.push(result.trigger_word);
 
-					socket.log("[REACTION] '" + result.key + "' has been excluded for " + result.timeout + "s");
-				}
-				return result.reaction.replace("@username", user['display-name']);
-			}
+                    setTimeout(function () {
+                        exclude_reactions.splice(exclude_reactions.indexOf(result.trigger_word), 1);
+                        socket.log("[REACTION] '" + result.trigger_word + "' has been removed from the exclusion list");
+                    }, result.timeout * 1000);
+
+                    socket.log("[REACTION] '" + result.trigger_word + "' has been excluded for " + result.timeout + "s");
+                }
+                return result.reaction.replace("@username", user['display-name']);
+            }
         }
         return;
     }
-    catch (err){
+    catch (err) {
         console.error(err);
         return null;
     }
 }
 
-module.exports = {init, run}
+module.exports = { init, run }
--- www/reactions.php
+++ www/reactions.php
@@ -2,170 +2,176 @@
 require_once('src/php/header.php');
 
 // POST
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-  if($_POST['action'] == "add" && !empty($_POST['key']) && !empty($_POST['reaction'])){
-    $key = strtolower(addslashes(trim($_POST['key'])));
-    $reaction = addslashes(trim($_POST['reaction']));
-    $frequency = intval($_POST['frequency']);
-    $timeout = intval($_POST['timeout']);
-    db_query_no_result($db, "INSERT INTO reactions VALUES ('$key', '$reaction', '$frequency', '$timeout')");
-  }
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($_POST['action'] == "add" && !empty($_POST['trigger_word']) && !empty($_POST['reaction'])) {
+        $trigger_word = strtolower(addslashes(trim($_POST['trigger_word'])));
+        $reaction = addslashes(trim($_POST['reaction']));
+        $frequency = intval($_POST['frequency']);
+        $timeout = intval($_POST['timeout']);
+        db_query_no_result($db, "INSERT INTO reactions VALUES (NULL, '$trigger_word', '$reaction', '$frequency', '$timeout')");
+    }
 
-  if($_POST['action'] == "del" && !empty($_POST['key'])){
-    $key = addslashes(trim($_POST['key']));
-    db_query_no_result($db, "DELETE FROM `reactions` WHERE `key` = '$key'");
-  }
+    if ($_POST['action'] == "del" && !empty($_POST['id'])) {
+        $id = addslashes(trim($_POST['id']));
+        db_query_no_result($db, "DELETE FROM `reactions` WHERE `id` = '$id'");
+    }
 
-  if($_POST['action'] == "edit" && !empty($_POST['key'])){
-    $key = addslashes(trim($_POST['key']));
-    $reaction = addslashes(trim($_POST['value']));
-    $frequency = addslashes(trim($_POST['frequency']));
-    $timeout = addslashes(trim($_POST['timeout']));
-    db_query_no_result($db, "UPDATE `reactions` SET `reaction` = '$reaction', `frequency` = '$frequency', `timeout` = '$timeout' WHERE `key` = '$key'");
-  }
+    if ($_POST['action'] == "edit" && !empty($_POST['id'])) {
+        $id = addslashes(trim($_POST['id']));
+        $reaction = addslashes(trim($_POST['value']));
+        $frequency = addslashes(trim($_POST['frequency']));
+        $timeout = addslashes(trim($_POST['timeout']));
+        db_query_no_result($db, "UPDATE `reactions` SET `reaction` = '$reaction', `frequency` = '$frequency', `timeout` = '$timeout' WHERE `id` = '$id'");
+    }
 
-  header('Location: reactions.php');
-  exit();
+    header('Location: reactions.php');
+    exit();
 }
 
 
 $HTML = "";
 $result = db_query_raw($db, "SELECT * FROM reactions");
-while($row = mysqli_fetch_assoc($result)) {
+while ($row = mysqli_fetch_assoc($result)) {
     $HTML .= "
     <tr>
-        <td>".$row["key"]."</td>
-        <td id='text_".$row["key"]."'>".$row["reaction"]."</td>
-        <td id='freq_".$row["key"]."' class='text-center'>".$row["frequency"]."</td>
-        <td id='time_".$row["key"]."' class='text-center'>".$row["timeout"]."</td>
+        <td>" . $row["trigger_word"] . "</td>
+        <td id='text_" . $row["id"] . "'>" . $row["reaction"] . "</td>
+        <td id='freq_" . $row["id"] . "' class='text-center'>" . $row["frequency"] . "</td>
+        <td id='time_" . $row["id"] . "' class='text-center'>" . $row["timeout"] . "</td>
         <td>
           <span class='pull-right'>
-            <button onClick='edit_entry(\"".$row["key"]."\")' class='btn btn-warning' type='button'><i class='glyphicon glyphicon-pencil'></i></button>
-            <button type='button' class='btn btn-danger' onclick='del_entry(\"".$row['key']."\")'><i class='glyphicon glyphicon-remove'></i></button>
+            <button onClick='edit_entry(\"" . $row["id"] . "\")' class='btn btn-warning' type='button'><i class='glyphicon glyphicon-pencil'></i></button>
+            <button type='button' class='btn btn-danger' onclick='del_entry(\"" . $row['id'] . "\")'><i class='glyphicon glyphicon-remove'></i></button>
           </span>
         </td>
     </tr>";
 }
 
 // Count
-$count = db_query($db, "SELECT COUNT(`key`) as value FROM reactions")['value'];
+$count = db_query($db, "SELECT COUNT(`id`) as value FROM reactions")['value'];
 
 ?>
 
 <!DOCTYPE html>
 <html>
-  <head>
-      <title>Reactions - <?php echo $bot_name; ?></title>
-      <?php include("src/html/header.html"); ?>
-  </head>
 
-  <body>
+<head>
+    <title>Reactions - <?php echo $bot_name; ?></title>
+    <?php include("src/html/header.html"); ?>
+</head>
+
+<body>
     <!-- TOP Navbar -->
     <?php include("src/php/navbar.php"); ?>
 
     <!-- Side bar-->
-    <?php include("src/html/sidebar.html"); ?> 
+    <?php include("src/html/sidebar.html"); ?>
 
     <!-- Main area -->
     <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-      <h1 class="page-header">Reactions (<?php echo $count;?>)
-        <span class='pull-right'>
-          <button type="button" class="btn btn-success" onclick='add_entry()'><i class="glyphicon glyphicon-plus"></i></button>
-        </span>
-      </h1>
+        <h1 class="page-header">Reactions (<?php echo $count; ?>)
+            <span class='pull-right'>
+                <button type="button" class="btn btn-success" onclick='add_entry()'><i class="glyphicon glyphicon-plus"></i></button>
+            </span>
+        </h1>
 
-      <!-- Add command -->
-      <table class="table table-hover table-condensed">
-        <thead>
-          <tr>
-              <th class="col-xs-2">Word</th>
-              <th class="col-xs-7">Reaction</th>
-              <th class="col-xs-1">Frequency (%)</th>
-              <th class="col-xs-1">Timeout (s)</th>
-              <th class="col-xs-1"></th>
-          </tr>
-        </thead>
-        <tbody>
-            <?php echo $HTML; ?>
-        </tbody>
-      </table>
+        <!-- Add command -->
+        <table class="table table-hover table-condensed">
+            <thead>
+                <tr>
+                    <th class="col-xs-2">Trigger</th>
+                    <th class="col-xs-7">Reaction</th>
+                    <th class="col-xs-1">Frequency (%)</th>
+                    <th class="col-xs-1">Timeout (s)</th>
+                    <th class="col-xs-1"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php echo $HTML; ?>
+            </tbody>
+        </table>
     </div>
 
     <!-- Footer -->
     <?php include("src/html/footer.html"); ?>
 
     <script>
-      $(document).ready(function() {
-        // Active the corresponding button in the navbar
-        document.getElementById("reactions").className="active"; 
-      });
-
-      function add_entry(){
-        Swal.fire({
-            title: "Add entry",
-            html:   "<form id='swal-form' method='post'>"+
-                    "<input type='hidden' name='action' value='add'>"+
-                    "<label>Trigger</label><input type='text' class='form-control' name='key' placeholder='Trigger' required><br/>"+
-                    "<label>Reaction</label><input type='text' class='form-control' name='reaction' placeholder='Reaction' required><br/>"+
-                    "<label>Frequency</label><input type='number' class='form-control' name='frequency' min=0 step=1 max=100 required><br/>"+
-                    "<label>Timeout</label><input type='number' class='form-control' name='timeout' min=0 step=1 required><br/>"+
-                    "</form>",
-            showCancelButton: true,
-            showConfirmButton: confirm,
-            focusConfirm: false,
-            allowOutsideClick: false,
-            width: "25%",
-            confirmButtonText: 'Add',
-            cancelButtonText: 'Cancel'
-        }).then((result) =>{
-            if(result.value)
-                document.getElementById('swal-form').submit();
+        $(document).ready(function() {
+            // Active the corresponding button in the navbar
+            document.getElementById("reactions").className = "active";
         });
-      }
 
-      function del_entry(key){
-        Swal({
-          title: "Delete '" + key + "' ?",
-          type: 'question',
-          showCancelButton: true,
-          confirmButtonColor: '#d33',
-          confirmButtonText: 'Yes',
-          cancelButtonText: 'No',
-          focusCancel: true
-        }).then((result) => {
-          if (result.value) {
-              $.post("reactions.php", { action : "del", key: key }, function(data){
-                  document.location.reload();
-              }); 
-          }
-        })
-      }
+        function add_entry() {
+            Swal.fire({
+                title: "Add entry",
+                html: "<form id='swal-form' method='post'>" +
+                    "<input type='hidden' name='action' value='add'>" +
+                    "<label>Trigger</label><input type='text' class='form-control' name='trigger_word' placeholder='Trigger' required><br/>" +
+                    "<label>Reaction</label><input type='text' class='form-control' name='reaction' placeholder='Reaction' required><br/>" +
+                    "<label>Frequency</label><input type='number' class='form-control' name='frequency' min=0 step=1 max=100 required><br/>" +
+                    "<label>Timeout</label><input type='number' class='form-control' name='timeout' min=0 step=1 required><br/>" +
+                    "</form>",
+                showCancelButton: true,
+                showConfirmButton: confirm,
+                focusConfirm: false,
+                allowOutsideClick: false,
+                width: "25%",
+                confirmButtonText: 'Add',
+                cancelButtonText: 'Cancel'
+            }).then((result) => {
+                if (result.value)
+                    document.getElementById('swal-form').submit();
+            });
+        }
 
-      function edit_entry(key){
-        text = document.getElementById("text_" + key).innerText;
-        freq = document.getElementById("freq_" + key).innerText;
-        time = document.getElementById("time_" + key).innerText;
-        Swal({
-            title: 'Editing : "' + key + '"',
-            type: 'info',
-            html: "<form id='swal-form' method='post'><br/>"+
-                  "<input type='hidden' name='action' value='edit'>"+
-                  "<input type='hidden' name='key' value='" + key + "'>"+
-                  "<label>Text</label><input class='form-control' type='text' name='value' value=\"" + text + "\"><br/>"+
-                  "<label>Frequency (%)</label><input class='form-control' type='number' name='frequency' min=0 step=1 max=100 value=\"" + freq + "\"><br/>"+
-                  "<label>Timeout (s)</label><input class='form-control' type='number' name='timeout' min=0 step=1 value=\"" + time + "\"><br/>"+
-                  "</form>",
-            showCancelButton: true,
-            focusConfirm: false,
-            allowOutsideClick: false,
-            width: "30%",
-            confirmButtonText: 'Edit',
-            cancelButtonText: 'Cancel'
-        }).then((result) =>{
-            if(result.value)
-                document.getElementById('swal-form').submit();
-        })
-      }
+        function del_entry(id) {
+            Swal({
+                title: "Delete '" + key + "' ?",
+                type: 'question',
+                showCancelButton: true,
+                confirmButtonColor: '#d33',
+                confirmButtonText: 'Yes',
+                cancelButtonText: 'No',
+                focusCancel: true
+            }).then((result) => {
+                if (result.value) {
+                    $.post("reactions.php", {
+                        action: "del",
+                        id: id
+                    }, function(data) {
+                        document.location.reload();
+                    });
+                }
+            })
+        }
+
+        function edit_entry(id) {
+            text = document.getElementById("text_" + id).innerText;
+            freq = document.getElementById("freq_" + id).innerText;
+            time = document.getElementById("time_" + id).innerText;
+            Swal({
+                title: 'Editing : "' + id + '"',
+                type: 'info',
+                html: "<form id='swal-form' method='post'><br/>" +
+                    "<input type='hidden' name='action' value='edit'>" +
+                    "<input type='hidden' name='id' value='" + id + "'>" +
+                    "<label>Text</label><input class='form-control' type='text' name='value' value=\"" + text + "\"><br/>" +
+                    "<label>Frequency (%)</label><input class='form-control' type='number' name='frequency' min=0 step=1 max=100 value=\"" + freq + "\"><br/>" +
+                    "<label>Timeout (s)</label><input class='form-control' type='number' name='timeout' min=0 step=1 value=\"" + time + "\"><br/>" +
+                    "</form>",
+                showCancelButton: true,
+                focusConfirm: false,
+                allowOutsideClick: false,
+                width: "30%",
+                confirmButtonText: 'Edit',
+                cancelButtonText: 'Cancel'
+            }).then((result) => {
+                if (result.value)
+                    document.getElementById('swal-form').submit();
+            })
+        }
     </script>
 
-</body></html>
+</body>
+
+</html>
