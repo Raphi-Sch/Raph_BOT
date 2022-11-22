04b364667d7d5a7764ce44cebe8bc7729f611240
Raphi_55 <35998957+Raphi-Sch@users.noreply.github.com>
2022-11-22 13:56:06 +0100

    [refact] Remove old function call

--- core/moderator/moderator_runner.js
+++ core/moderator/moderator_runner.js
@@ -1,12 +1,10 @@
-const tools = require("../tools")
-const db = require("../db")
 const socket = require("../socket")
+const API_URL = require("../../config.json").API_URL;
 
 const { re } = require("@babel/core/lib/vendor/import-meta-resolve");
 
 async function run_moderator(user, message) {
     const words = message.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/['"]+/g, ' ').split(" ");
-    //const result = await query_moderator(words);
     const result = await api_moderator(words);
 
     try {
@@ -23,16 +21,6 @@ async function run_moderator(user, message) {
         console.error(err);
         return null;
     }
-}
-
-async function query_moderator(words) {
-    const trigger_word_in = words.map(() => "?").join(",")
-    const res = await db.query(`SELECT mod_action, explanation
-                                  FROM moderator
-                                  WHERE moderator.trigger_word IN (${trigger_word_in})
-                                  ORDER BY RAND() LIMIT 1`, words);
-
-    return tools.first_of_array(res);
 }
 
 async function api_moderator(words){
