6eabede5c14fab331700489fcc5e36e3d7eaeecd
RaphaelSch <raphael.scharff@gmail.com>
2023-03-19 15:34:42 +0100

    [feat] Update version

--- core/core.js
+++ core/core.js
@@ -6,7 +6,7 @@ const moderator = require("./moderator/moderator")
 const reaction = require("./reaction/reaction")
 const shout = require("./shout/shout")
 
-const version = "v5.10.0";
+const version = "v5.10.1";
 
 config.load().then(() => {
     // UI
