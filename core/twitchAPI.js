b2dadad14e19c90b2eb2a411cc61fcf26604eada
RaphaelSch <raphael.scharff@gmail.com>
2023-03-19 12:38:01 +0100

    [feat] Init + timeoutUser

--- core/twitchAPI.js
+++ core/twitchAPI.js
@@ -1,11 +1,25 @@
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
-async function getUserId(userName, clientID, authorizationToken) {
+const configAPI = {
+    authorizationToken : null,
+    clientID : null,
+    broadcasterID : null,
+    moderatorID : null
+}
+
+async function init(broadcasterName, botName, clientID, authorizationToken){
+    configAPI.authorizationToken = authorizationToken;
+    configAPI.clientID = clientID;
+    configAPI.broadcasterID = await getUserId(broadcasterName);
+    configAPI.moderatorID = await getUserId(botName);
+}
+
+async function getUserId(userName) {
     const response = await fetch(`https://api.twitch.tv/helix/users?login=${userName}`, {
         method: "get",
         headers: {
-            "Authorization": `Bearer ${authorizationToken}`,
-            "Client-Id": clientID
+            "Authorization": `Bearer ${configAPI.authorizationToken}`,
+            "Client-Id": configAPI.clientID
         }
     })
 
@@ -18,23 +32,21 @@ async function getUserId(userName, clientID, authorizationToken) {
     }
 }
 
-async function banUser(broadcasterID, botID, clientID, authorizationToken, userID, reason) {
+async function banUser(userID, reason) {
     const body = {
-        data: [
-            {
-                user_id: userID,
-                reason: reason
-            }
-        ]
+        data: {
+            user_id: userID,
+            reason: reason
+        }
     }
 
-    const response = await fetch(`https://api.twitch.tv/helix/moderation/bans?broadcaster_id=${broadcasterID}&moderator_id=${botID}`, {
+    const response = await fetch(`https://api.twitch.tv/helix/moderation/bans?broadcaster_id=${configAPI.broadcasterID}&moderator_id=${configAPI.moderatorID}`, {
         method: "post",
         body: JSON.stringify(body),
         headers: {
             "Content-Type": "application/json",
-            "Authorization": `Bearer ${authorizationToken}`,
-            "Client-Id": clientID
+            "Authorization": `Bearer ${configAPI.authorizationToken}`,
+            "Client-Id": configAPI.clientID
         }
     })
 
@@ -46,4 +58,31 @@ async function banUser(broadcasterID, botID, clientID, authorizationToken, userI
     }
 }
 
-module.exports = { getUserId, banUser }
+async function timeoutUser(userID, reason, duration) {
+    const body = {
+        data: {
+            user_id: userID,
+            reason: reason,
+            duration: duration
+        }
+    }
+
+    const response = await fetch(`https://api.twitch.tv/helix/moderation/bans?broadcaster_id=${configAPI.broadcasterID}&moderator_id=${configAPI.moderatorID}`, {
+        method: "post",
+        body: JSON.stringify(body),
+        headers: {
+            "Authorization": `Bearer ${configAPI.authorizationToken}`,
+            "Client-Id": configAPI.clientID,
+            "Content-Type": "application/json"
+        }
+    })
+
+    if (response.ok) {
+        return true;
+    } else {
+        console.error("[TWITCH API] Error while executing function timeoutUser() -> HTTP ERROR : " + response.status + " (" + response.statusText + ")");
+        return null;
+    }
+}
+
+module.exports = { init, getUserId, banUser, timeoutUser }
