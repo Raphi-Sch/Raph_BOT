d14017839e06b3b4b886da91c71a79b7a1d75faa
RaphaelSch <raphael.scharff@gmail.com>
2023-03-26 16:09:53 +0200

    [refact] Cleanup

--- www/api/commands.php
+++ www/api/commands.php
@@ -1,7 +1,10 @@
 <?php
 
 require_once('../src/php/db.php');
-require_once('./commands-tanks.php');
+require_once('./commands/audio.php');
+require_once('./commands/tanks.php');
+
+
 header('Content-Type: application/json');
 
 $db = db_connect("../../config.json");
@@ -134,64 +137,4 @@ function list_alias(mysqli $db)
     }
 
     return $result;
-}
-
-function list_audio(mysqli $db)
-{
-    $SQL_query = "SELECT * FROM commands_audio ORDER BY `name` ASC";
-    $data = db_query_raw($db, $SQL_query);
-
-    $result = array();
-    $count = 0;
-
-    while ($row = mysqli_fetch_assoc($data)) {
-        $result += array($count => ["id" => $row['id'], "name" => $row["name"], "trigger_word" => $row["trigger_word"], "volume" => $row["volume"], "timeout" => $row["timeout"], "file" => $row['file'], "active" => $row['active'], 'mod_only' => $row['mod_only'], 'sub_only' => $row['sub_only']]);
-        $count++;
-    }
-
-    return $result;
-}
-
-function list_audio_text(mysqli $db){
-    $result = "Commandes sonore : ";
-    $first = true;
-
-    $data = db_query_raw($db, "SELECT * FROM commands_audio WHERE active = 1 AND sub_only = 0 AND mod_only = 0 ORDER BY `trigger_word` ASC");
-    while ($row = mysqli_fetch_assoc($data)) {
-        if ($first){
-            $result .= "!" . $row['trigger_word'];
-            $first = false;
-        }
-        else
-            $result .= ", !" . $row['trigger_word'];
-    }
-
-    return ['response_type' => 'text', 'value' => $result];
-}
-
-function request_audio(mysqli $db, string $command, array $excluded_audio)
-{
-    $SQL_params_type = "";
-    $SQL_params = array();
-    $trigger_word_not_in = "";
-
-    // Command
-    $SQL_params_type .= "s";
-    array_push($SQL_params, $command);
-
-    // Build audio not in
-    if (count($excluded_audio) > 0) {
-        $excluded_audio_count = count($excluded_audio);
-        $trigger_word_not_in = " AND trigger_word NOT IN (" . join(',', array_fill(0, $excluded_audio_count, '?')) . ")";
-        $SQL_params_type .= str_repeat('s', $excluded_audio_count);
-        $SQL_params = array_merge($SQL_params, $excluded_audio);
-    }
-
-    $SQL_query = "SELECT * FROM commands_audio WHERE trigger_word = ? AND active = 1 $trigger_word_not_in";
-    $result = db_query($db, $SQL_query, $SQL_params_type, $SQL_params);
-
-    if ($result == null)
-        return null;
-    else
-        return ['response_type' => 'audio', "id" => $result['id'], 'trigger_word' => $result['trigger_word'], 'timeout' => $result['timeout'], 'file' => $result['file'], 'name' => $result['name'], 'volume' => $result['volume'], 'mod_only' => $result['mod_only'], 'sub_only' => $result['sub_only']];
 }
