391c1c70bd323ef7e052f0629cf7c894bdde6d54
Raphi_55 <35998957+Raphi-Sch@users.noreply.github.com>
2023-04-14 11:01:50 +0200

    [feat] Version upgrade

--- core/core.js
+++ core/core.js
@@ -6,7 +6,7 @@ const moderator = require("./moderator/moderator")
 const reaction = require("./reaction/reaction")
 const shout = require("./shout/shout")
 
-const version = "v5.11.3";
+const version = "v5.12.0";
 
 config.load().then(() => {
     // UI
