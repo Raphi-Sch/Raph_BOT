ab83ff8e7720f5bb4da760b90a39f2d254b23629
RaphaelSch <raphael.scharff@gmail.com>
2023-04-14 18:51:47 +0200

    [refact] Clean up

--- www/api/moderator.php
+++ www/api/moderator.php
@@ -32,43 +32,6 @@ switch ($_SERVER["REQUEST_METHOD"]) {
         break;
 }
 
-function request(mysqli $db, $message)
-{
-    $result = null;
-
-    // Convert array to string
-    if (is_array($message))
-        $message = implode(" ", $message);
-
-    $message = trim(strtolower($message));
-
-    $SQL_query = "SELECT trigger_word FROM moderator";
-    $data = db_query_raw($db, $SQL_query);
-
-    $current_expression = "";
-    $action_trigger = false;
-
-    while($row = $data->fetch_assoc()){
-        $current_expression = $row['trigger_word'];
-        if(strrpos($message, $current_expression) !== false){
-            $action_trigger = true;
-            break;
-        }
-    }
-
-    if($action_trigger){
-        $SQL_query = "SELECT * FROM moderator WHERE trigger_word = ?";
-        $SQL_params_type = "s";
-        $SQL_values = $current_expression;
-        $result = db_query($db, $SQL_query, $SQL_params_type, $SQL_values);
-    }
-
-    if ($result == null)
-        return ['mod_action' => null, 'explanation' => null, 'duration' => null, 'reason' => null, 'trigger_word' => null];
-    else
-        return $result;
-}
-
 function get_list(mysqli $db)
 {
     $SQL_query = "SELECT * FROM moderator ORDER BY trigger_word ASC";
@@ -83,4 +46,36 @@ function get_list(mysqli $db)
     }
 
     return $result;
+}
+
+function request(mysqli $db, $message)
+{
+    $result = null;
+
+    // Convert array to string
+    if (is_array($message))
+        $message = implode(" ", $message);
+
+    $message = trim(strtolower($message));
+
+    $current_expression = "";
+    $action_trigger = false;
+    $trigger_data = db_query_raw($db, "SELECT trigger_word FROM moderator");
+    
+    while($row = $trigger_data->fetch_assoc()){
+        $current_expression = $row['trigger_word'];
+        if(strrpos($message, $current_expression) !== false){
+            $action_trigger = true;
+            break;
+        }
+    }
+
+    if($action_trigger){
+        $result = db_query($db, "SELECT * FROM moderator WHERE trigger_word = ?", "s", $current_expression);
+    }
+
+    if ($result == null)
+        return ['mod_action' => null, 'explanation' => null, 'duration' => null, 'reason' => null, 'trigger_word' => null];
+    else
+        return $result;
 }
