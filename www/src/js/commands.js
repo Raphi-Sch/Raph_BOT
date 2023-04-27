73b7d57afde5f873a19a5641f386d2705dbb00e1
Raphi_55 <35998957+Raphi-Sch@users.noreply.github.com>
2023-04-27 15:04:53 +0200

    [refact] Consolidating

--- www/src/js/commands.js
+++ www/src/js/commands.js
@@ -398,27 +398,16 @@ function listTTS(reload = false) {
             LIST.innerHTML = "";
 
             data.forEach(element => {
-                let editFunction = null;
-                let displayValue = element.value;
-
-                switch (element.type) {
-                    case 0:
-                        editFunction = editText;
-                        break;
-                    case 1:
-                        editFunction = editBool;
-                        displayValue = element.value == '1' ? "Enabled" : "Disabled";
-                        break;
-                    case 2:
-                        editFunction = editNumber;
-                        break;
-                }
-
                 const TR = document.createElement('tr');
+
+                if(element.type == 1)
+                    displayValue = (element.value == '1' ? "Enabled" : "Disabled");
+                else    
+                    displayValue = element.value;
 
                 TR.appendChild(createTableData(element.id, 'col-xs-2'));
                 TR.appendChild(createTableData(displayValue, 'col-xs-5'));
-                TR.appendChild(createButtonGroup(createButton("btn btn-warning", "glyphicon glyphicon-pencil", () => editFunction(element))));
+                TR.appendChild(createButtonGroup(createButton("btn btn-warning", "glyphicon glyphicon-pencil", () => editTTS(element))));
 
                 LIST.appendChild(TR);
             })
@@ -431,72 +420,39 @@ function listTTS(reload = false) {
     })
 }
 
-function editText(element) {
-    Swal.fire({
-        title: `Editing : ${element.id}`,
-        icon: 'info',
-        html: "<form id='swal-form'>" +
-            "<input type='hidden' name='action' value='edit-tts-config'>" +
-            `<input type='hidden' name='id' value='${element.id}'>` +
-            `<input class='form-control' type='text' name='value' value="${element.value}"></form>`,
-        showCancelButton: true,
-        focusConfirm: false,
-        allowOutsideClick: false,
-        width: "30%",
-        confirmButtonText: 'Edit',
-        cancelButtonText: 'Cancel'
-    }).then((result) => {
-        if (result.value) {
-            const FORM_DATA = $(document.getElementById('swal-form')).serializeArray();
-            $.post('src/php/POST_commands.php', FORM_DATA).done(function () {
-                listTTS(true);
-            });
-        }
-    })
-}
+function editTTS(element) {
+    let input = "";
+    let didOpen = null;
+    switch(element.type){
+        case 0:
+            input = `<input class='form-control' type='text' name='value' value="${element.value}">`;
+            break;
 
-function editBool(element) {
+        case 1:
+            input = `<select id='swal-select' name='value' class='form-control'><option value=0>Disable</option><option value=1>Enable</option></select>`;
+            didOpen = () => { document.getElementById('swal-select').value = element.value; }
+            break;
+
+        case 2:
+            input = `<input class='form-control' type='number' name='value' step=1 min=1 max=1000 value="${element.value}">`;
+            break;
+    }
+
     Swal.fire({
         title: `Editing : ${element.id}`,
         icon: 'info',
-        html: "<form id='swal-form'>" +
-            "<input type='hidden' name='action' value='edit-tts-config'>" +
-            `<input type='hidden' name='id' value='${element.id}'>` +
-            "<select id='swal-select' name='value' class='form-control'>" +
-            "<option value=0>Disable</option>" +
-            "<option value=1>Enable</option>" +
-            "</select></form>",
+        html: `<form id='swal-form'>
+            <input type='hidden' name='action' value='edit-tts-config'>
+            <input type='hidden' name='id' value='${element.id}'>
+            ${input}
+            </form>`,
         showCancelButton: true,
         focusConfirm: false,
         allowOutsideClick: false,
         width: "30%",
         confirmButtonText: 'Edit',
         cancelButtonText: 'Cancel',
-        didOpen: () => { document.getElementById('swal-select').value = element.value; }
-    }).then((result) => {
-        if (result.value) {
-            const FORM_DATA = $(document.getElementById('swal-form')).serializeArray();
-            $.post('src/php/POST_commands.php', FORM_DATA).done(function () {
-                listTTS(true);
-            });
-        }
-    })
-}
-
-function editNumber(element) {
-    Swal.fire({
-        title: `Editing : ${element.id}`,
-        icon: 'info',
-        html: "<form id='swal-form'>" +
-            "<input type='hidden' name='action' value='edit-tts-config'>" +
-            `<input type='hidden' name='id' value='${element.id}'>` +
-            `<input class='form-control' type='number' name='value' step=1 min=1 max=1000 value="${element.value}"></form>`,
-        showCancelButton: true,
-        focusConfirm: false,
-        allowOutsideClick: false,
-        width: "30%",
-        confirmButtonText: 'Edit',
-        cancelButtonText: 'Cancel',
+        didOpen: didOpen
     }).then((result) => {
         if (result.value) {
             const FORM_DATA = $(document.getElementById('swal-form')).serializeArray();
