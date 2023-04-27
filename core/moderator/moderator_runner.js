57c2c51e00d1e4b245fd6265db8d545bea9a92ed
Raphi_55 <35998957+Raphi-Sch@users.noreply.github.com>
2023-04-27 12:23:38 +0200

    [feat] WarnUser

--- core/moderator/moderator_runner.js
+++ core/moderator/moderator_runner.js
@@ -8,15 +8,15 @@ const { re } = require("@babel/core/lib/vendor/import-meta-resolve");
 
 async function runModerator(user, message, twitchAPI) {
     const fullCommand = tools.parseCommand(message, config.cmd_prefix);
-    if(fullCommand){
-        return command(fullCommand, user, message, twitchAPI);
+    if (fullCommand) {
+        return command(fullCommand, user, twitchAPI);
     }
-    else{
+    else {
         return checkMessage(user, message, twitchAPI);
     }
 }
 
-async function checkMessage(user, message, twitchAPI){
+async function checkMessage(user, message, twitchAPI) {
     const result = await moderatorAPI.checkMessage(message);
 
     try {
@@ -46,22 +46,47 @@ async function checkMessage(user, message, twitchAPI){
     }
 }
 
-async function command(command, user, message, twitchAPI){
-    switch(command[1]){
-        case 'warn':
-            return await warnUser(command, twitchAPI);
+async function command(command, user, twitchAPI) {
+    // Only moderators or broadcaster
+    if (user && (user.mod || user.username === config.twitch_channel.toLowerCase())) {
+        switch (command[1]) {
+            case 'warn':
+                return await warnUser(command, user, twitchAPI);
+        }
     }
-
     return null;
 }
 
-async function warnUser(command, twitchAPI){
-    const user = await twitchAPI.getUserInfo(command[2]);
+async function warnUser(command, moderator, twitchAPI) {
+    const userInput = command[2].trim();
+    const user = await twitchAPI.getUser(userInput);
 
-    if(user !== null)
+    if (user !== null) {
         result = await moderatorAPI.warnUser(user);
+        if(result !== null){
+            switch (result.count){
+                case 1:
+                    return `${result.username} : This is your first and last warning before being timeout, be careful !`;
+                case 2:
+                    twitchAPI.timeoutUser(user.id, "Too much warnings", 30);
+                    return `${result.username} : Go touch some grass (timeout 30s)`;
+                case 3:
+                    twitchAPI.timeoutUser(user.is, "3rd warning ...", 60);
+                    return `${result.username} : Seriously, you again ? (timeout 1min)`;
+                case 4:
+                    twitchAPI.timeoutUser(user.is, "4th and last warning ...", 600);
+                    return `${result.username} : No comment (timeout 10min)`;
+                case 5:
+                    twitchAPI.banUser(user.id, "Bye bye.")
+                    return `${result.username} : Looks like you will touch grass forever.`;
+            }
+        }       
+    }
+    else {
+        return `${moderator['display-name']} : User '${userInput}' doesn't exist.`;
+    }
 
-    return `${result.user} this is your ${result.count} warning, be careful !`;
+    return null;
 }
 
 module.exports = { runModerator }
