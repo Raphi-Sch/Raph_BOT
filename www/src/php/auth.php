ee07db33165e7846249a0622e30a346d2c3b007d
RaphaelSch <raphael.scharff@gmail.com>
2023-04-29 13:41:02 +0200

    [fix] Lower and Upper cased headers, more debug

--- www/src/php/auth.php
+++ www/src/php/auth.php
@@ -4,17 +4,18 @@ function unauhorized($message = null)
 {
     header("HTTP/1.1 401 Unauthorized");
     header('Content-Type: application/json');
+    error_log("HTTP/1.1 401 Unauthorized : " . $message);
     echo json_encode(['error' => 'Unauhorized', 'status' => 401, 'message' => $message]);
     exit();
 }
 
 if(isset($_COOKIE['raphbot_api_client']) && isset($_COOKIE['raphbot_api_token'])){
     if (empty($_COOKIE['raphbot_api_token'])) {
-        unauhorized("Invalid Authorization token");
+        unauhorized("Invalid Authorization token (cookie)");
     }
 
     if (empty($_COOKIE['raphbot_api_client'])) {
-        unauhorized("Invalid client");
+        unauhorized("Invalid client (cookie)");
     }
 
     $api_client = $_COOKIE['raphbot_api_client'];
@@ -22,23 +23,24 @@ if(isset($_COOKIE['raphbot_api_client']) && isset($_COOKIE['raphbot_api_token'])
 }
 else{
     $headers = apache_request_headers();
+    $headers = array_change_key_case($headers, CASE_LOWER); // Fetch send header in lowercase while browser doesn't
 
-    if (!isset($headers['Authorization']) || empty($headers['Authorization'])) {
+    if (!isset($headers['authorization']) || empty($headers['authorization'])) {
         unauhorized("Invalid Authorization token");
     }
     
-    $headers['Authorization'] = explode(" ", $headers['Authorization'], 2);
+    $headers['authorization'] = explode(" ", $headers['authorization'], 2);
     
-    if ($headers['Authorization'][0] !== "Bearer"){
+    if ($headers['authorization'][0] !== "Bearer"){
         unauhorized("Wrong token type, must be 'Bearer'");
     }
     
-    if (!isset($headers['Client']) || empty($headers['Client'])) {
+    if (!isset($headers['client']) || empty($headers['client'])) {
         unauhorized("Invalid client");
     }
 
-    $api_client = $headers['Client'];
-    $api_token = $headers['Authorization'][1];
+    $api_client = $headers['client'];
+    $api_token = $headers['authorization'][1];
     unset($headers);
 }
 
