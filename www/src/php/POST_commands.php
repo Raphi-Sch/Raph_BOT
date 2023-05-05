d1fde20751dfb4cb977c5a795d7d94ac6771f66c
Raphi_55 <35998957+Raphi-Sch@users.noreply.github.com>
2023-05-05 11:39:48 +0200

    [refact] Moving header into files

--- www/src/php/POST_commands.php
+++ www/src/php/POST_commands.php
@@ -1,6 +1,15 @@
 <?php
 
-require_once("./header-post.php");
+require_once('db.php');
+require_once('access.php');
+require_once('functions.php');
+
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    error_post();
+    exit();
+}
+
+$db = db_connect("../../../config.json");
 
 switch ($_POST['action']) {
     case "add-audio":
