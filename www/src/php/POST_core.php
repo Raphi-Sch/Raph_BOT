76c291c18b8cbbacf46c87a96ddbd8318c7f7f70
RaphaelSch <raphael.scharff@gmail.com>
2023-05-09 22:52:40 +0200

    [fix] Clear lastest.log at start

--- www/src/php/POST_core.php
+++ www/src/php/POST_core.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 
-$core_directory = dirname(__DIR__)."/../../core/";
+$core_directory = dirname(__DIR__) . "/../../core";
 
-if(isset($_POST) && $_POST['action'] == "start"){
-    shell_exec($core_directory.'lastest.log; node '.$core_directory.'core.js > '.$core_directory.'debug.log 2>&1');
+if (isset($_POST) && $_POST['action'] == "start") {
+    shell_exec("> $core_directory/lastest.log; node $core_directory/core.js > $core_directory/debug.log 2>&1");
 }
 
 exit();
