064f67d376c9f2b1b3c79d8e0c775c0e50178ba7
Raphi_55 <35998957+Raphi-Sch@users.noreply.github.com>
2023-05-09 12:57:53 +0200

    [feat] Screen not longer needed

--- www/src/php/POST_core.php
+++ www/src/php/POST_core.php
@@ -3,7 +3,7 @@
 $core_directory = dirname(__DIR__)."/../../core/";
 
 if(isset($_POST) && $_POST['action'] == "start"){
-    shell_exec('screen -dmS bot clear; > '.$core_directory.'lastest.log; node '.$core_directory.'core.js > '.$core_directory.'debug.log 2>&1');
+    shell_exec($core_directory.'lastest.log; node '.$core_directory.'core.js > '.$core_directory.'debug.log 2>&1');
 }
 
 exit();
