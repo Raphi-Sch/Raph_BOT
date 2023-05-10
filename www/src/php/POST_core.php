06ab9d56dc205169ad770c1cd1c151df849232da
Raphi_55 <35998957+Raphi-Sch@users.noreply.github.com>
2023-05-10 10:10:33 +0200

    [fix] PHP script waiting for result

--- www/src/php/POST_core.php
+++ www/src/php/POST_core.php
@@ -1,9 +1,14 @@
 <?php
+require_once('access.php');
 
-$core_directory = dirname(__DIR__) . "/../../core";
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    error_post();
+    exit();
+}
 
 if (isset($_POST) && $_POST['action'] == "start") {
-    shell_exec("> $core_directory/lastest.log; node $core_directory/core.js > $core_directory/debug.log 2>&1");
+    $core_directory = dirname(__DIR__) . "/../../core";
+    exec("> $core_directory/lastest.log; node $core_directory/core.js > $core_directory/debug.log 2>&1 &");
 }
 
 exit();
