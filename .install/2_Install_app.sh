41992d94e8b3ad4c2a307a80813cd8c689c9d360
Raphaël <35998957+Raphi-Sch@users.noreply.github.com>
2024-04-14 13:48:08 +0200

    [fix] log

--- .install/2_Install_app.sh
+++ .install/2_Install_app.sh
@@ -10,7 +10,7 @@ pause
 # Apache2
 echo "Configuring Apache2 ..."
 if [ -f "/etc/apache2/sites-enabled/raph_bot.conf" ]; then
-    echo -e "\t- Core config file already exist."
+    echo -e "\t- Site config file already exist."
 else 
     sudo cp raph_bot_virtualhost.conf /etc/apache2/sites-available/raph_bot.conf
     sudo nano /etc/apache2/sites-available/raph_bot.conf
