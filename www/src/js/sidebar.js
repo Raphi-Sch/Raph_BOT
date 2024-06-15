81103b3834cdcff18896631b9c3a3642b4307919
Raphaël <35998957+Raphi-Sch@users.noreply.github.com>
2024-06-16 00:24:56 +0200

    [feat] Optimise disabled plugin

--- www/src/js/sidebar.js
+++ www/src/js/sidebar.js
@@ -3,20 +3,13 @@ $.ajax({
     type: "GET",
     dataType: "json",
     success: function (data) {
-        let title_enable = false;
+        for (const id in data)
+            if (data[id] != "1" && document.getElementById(id)) {
+                let element = document.getElementById(id);
+                element.classList.add("disabled");
+                element.childNodes[0].href = "javascript: void(0)";
 
-        for (const id in data) {
-            if (data[id] == "1") {
-                title_enable = true;
-                if(document.getElementById(id))
-                    document.getElementById(id).classList.remove("hidden");
             }
-        }
-
-        if(title_enable)
-            document.getElementById("plugin_title").classList.remove("hidden");
-        else
-            document.getElementById("plugin_title").classList.add("hidden");
     },
     error: (result, status, error) => errorAPI(result, status, error)
 })
