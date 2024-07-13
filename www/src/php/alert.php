9318a62340dae79f05fefd5c0c086a0b40451195
Raphael <raphael.scharff@gmail.com>
2024-07-13 22:19:36 +0200

    [fix] Alert

--- www/src/php/alert.php
+++ www/src/php/alert.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['alert'])) {
 <script>
     $(document).ready(function() {
         <?php 
-            if (!empty($$_SESSION['alert'])){
+            if (!empty($_SESSION['alert'])){
                 echo "Swal.fire({icon: \"" . $_SESSION['alert'][0] . "\", title: \"" . $_SESSION['alert'][1] . "\", text: \"" . $_SESSION['alert'][2] . "\", toast: \"" . $_SESSION['alert'][3] . "\"});"; 
                 $_SESSION['alert'] = false; // Clear alert
             }
