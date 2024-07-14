3bf452febcb4873e47f70a896b5758e7ceaf73c2
Raphael <raphael.scharff@gmail.com>
2024-07-14 12:31:42 +0200

    [refact] Deduplication of 'canUseCommand', [fix] TTS queue

--- core/command/commands_runner.js
+++ core/command/commands_runner.js
@@ -8,7 +8,7 @@ const { timeTrigger } = require('./commands');
 let excludedTanks = [];
 
 const tts = {
-    timeoutRunning : false,
+    timeoutRunning: false,
     queue: []
 }
 
@@ -19,12 +19,10 @@ const audio = {
 }
 
 async function runCommand(user, message) {
-    let result = null;
-
-    // Parse command
     const fullCommand = tools.parseCommand(message, config.cmd_prefix);
 
     if (fullCommand) {
+        let result = null;
         // Call bot API
         let command = await queryAPI(fullCommand);
 
@@ -37,7 +35,7 @@ async function runCommand(user, message) {
         // Default result is true, to stop processing even if command is unknown. Maybe the command is for another bot
         result = true;
 
-        if (command !== null && command.response_type !== null) {
+        if (command !== null && command.response_type !== null && canUseCommand(command, user)) {
             switch (command.response_type) {
                 case "stop":
                     result = runStop(user);
@@ -72,10 +70,12 @@ async function runCommand(user, message) {
 
         // Log if user issued command
         if (user && result !== false)
-            socket.log(`[COMMANDS] '${fullCommand[1]}' has been used by '${user['display-name']}'`);
+            socket.log(`[COMMANDS] '${fullCommand[1]}' used by '${user['display-name']}'`);
+
+        return result;
     }
 
-    return result;
+    return null;
 }
 
 async function queryAPI(fullCommand) {
@@ -133,7 +133,7 @@ function runText(command, user) {
     if (user)
         command.value = command.value.replace("@username", user['display-name']);
 
-    if (canUseCommand(command, user) && command.value !== null)
+    if (command.value !== null)
         return command.value;
 
     return false;
@@ -153,88 +153,62 @@ function runTankRandom(command, user) {
 }
 
 function runAudio(command, user) {
-    if (canUseCommand(command, user)) {
-        if (command.timeout > 0) {
-            audio.excluded.push(command.trigger_word);
+    if (command.timeout > 0) {
+        audio.excluded.push(command.trigger_word);
 
-            // Specific timeout
-            setTimeout(function () {
-                audio.excluded.splice(audio.excluded.indexOf(command.trigger_word), 1);
-                socket.log(`[AUDIO] '${command.name}' has been removed from the exclusion list`);
-            }, parseInt(command.timeout) * 1000);
-        }
-
-        // Global timeout
-        audio.timeoutStart = Date.now();
-        audio.timeoutTotal = parseInt(command.global_timeout);
+        // Specific timeout
         setTimeout(function () {
-            audio.timeoutStart = 0
-            socket.log(`[AUDIO] Global timeout over.`);
-        }, audio.timeoutTotal * 1000);
-
-        socket.log(`[AUDIO] '${command.name}' has been played by '${user['display-name']}' (timeout : ${tools.timeoutToString(command.timeout)})`);
-        socket.log(`[AUDIO] Global timeout set (${tools.timeoutToString(audio.timeoutTotal)})`);
-
-        socket.playAudio(command);
-        return true;
+            audio.excluded.splice(audio.excluded.indexOf(command.trigger_word), 1);
+            socket.log(`[AUDIO] '${command.name}' has been removed from the exclusion list`);
+        }, parseInt(command.timeout) * 1000);
     }
-    return false;
+
+    // Global timeout
+    audio.timeoutStart = Date.now();
+    audio.timeoutTotal = parseInt(command.global_timeout);
+    setTimeout(function () {
+        audio.timeoutStart = 0
+        socket.log(`[AUDIO] Global timeout over.`);
+    }, audio.timeoutTotal * 1000);
+
+    socket.log(`[AUDIO] '${command.name}' has been played by '${user['display-name']}' (timeout : ${tools.timeoutToString(command.timeout)})`);
+    socket.log(`[AUDIO] Global timeout set (${tools.timeoutToString(audio.timeoutTotal)})`);
+
+    socket.playAudio(command);
+    return true;
+
 }
 
 function runTextToSpeech(command, user) {
     // TTS issued by user
     if (user && command.type == 'user') {
-        if (!canUseCommand(command, user)) {
-            if (config.debug_level >= 1) {
-                console.error(`[TTS] Access denied to ${user['display-name']}`);
-            }
-            return false;
-        }
-
         command.value = command.value.replace("@username", tools.simplifyUsername(user['display-name']));
 
-        if(!tts.timeoutRunning){
-            // Play directly
-            tools.TTS(config, socket, command.value, user['display-name']);
-            socket.log(`[TTS] Timeout for ${tools.timeoutToString(parseInt(command.timeout))}, next message will be queued during the timeout`);
+        // Push in queue
+        tts.queue.push({
+            'command': command,
+            'user': user['display-name']
+        });
 
-            // Timeout
-            tts.timeoutRunning = true;
-            setTimeout(() => {
-                runTextToSpeechTimeout(command.timeout);
-                tts.timeoutRunning = false;
-            }, parseInt(command.timeout) * 1000);
-
-            // DEBUG
-            if(config.debug_level >= 2){
-                console.error(`[TTS] Played directly (queue length is 0)`);
-            }
-
-            return true;
-        }
-        else{
+        // Run queue
+        if (runTextToSpeechQueue()) {
             // Play is delayed
-            tts.queue.push({
-                'text': command.value, 
-                'user': user['display-name'],
-                'twitchText': command.text_playing
-            });
-
             socket.log(`[TTS] Message added to queue (current length : ${tts.queue.length})`);
 
             // DEBUG
-            if(config.debug_level >= 2){
-                console.error(`[TTS] Queue length : ${tts.queue.length}`);
+            if (config.debug_level >= 2) {
+                console.error(`[TTS] Queue :`);
+                console.error(tts.queue.length);
             }
 
-            if(command.text_timeout != null){
+            if (command.text_timeout != null) {
                 command.text_timeout = command.text_timeout.replace("@username", tools.simplifyUsername(user['display-name']));
                 command.text_timeout = command.text_timeout.replace("@timeout", tools.timeoutToString(tts.queue.length * command.timeout));
                 return command.text_timeout;
             }
-
-            return true;
         }
+
+        return true;
     }
 
     // TTS issued by internal code
@@ -249,21 +223,27 @@ function runTextToSpeech(command, user) {
     return true; // No text output, but command successful
 }
 
-function runTextToSpeechTimeout(timeout){
-    let current = tts.queue[0];
-    tts.queue.shift();
-    tools.TTS(config, socket, current.text, current.user);
-    socket.log(`[TTS] Playing from queue (${tts.queue.length} left)`);
-    
-    setTimeout(() => {
-        if(tts.queue.length > 0){
+function runTextToSpeechQueue() {
+    if (!tts.timeoutRunning) {
+        let current = tts.queue[0];
+
+        if (current != null) {
+            tools.TTS(config, socket, current.command.value, current.user);
+            socket.log(`[TTS] Playing from queue (${tts.queue.length} left)`);
             tts.timeoutRunning = true;
-            runTextToSpeechTimeout();
         }
-        else {
-            tts.timeoutRunning = false;
+
+        tts.queue.shift();
+
+        if (tts.timeoutRunning) {
+            setTimeout(() => {
+                tts.timeoutRunning = false;
+                runTextToSpeechQueue();
+            }, parseInt(current.command.timeout) * 1000);
         }
-    }, parseInt(timeout) * 1000);
+        return false;
+    }
+    return true;
 }
 
 function getTimeLeft(obj) {
