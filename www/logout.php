f1c36e3a44f84916819e6e52da5faed63b33e4fd
Raphael <raphael.scharff@gmail.com>
2025-02-08 17:22:10 +0100

    [fix] set_cookie parameters

--- www/logout.php
+++ www/logout.php
@@ -2,8 +2,8 @@
     session_start();
     unset($_COOKIE['raphbot_api_client']); 
     unset($_COOKIE['raphbot_api_token']); 
-    setcookie('raphbot_api_client', null, -1, dirname($_SERVER['PHP_SELF'])); 
-    setcookie('raphbot_api_token', null, -1, dirname($_SERVER['PHP_SELF'])); 
+    setcookie('raphbot_api_client', '', -1, dirname($_SERVER['PHP_SELF'])); 
+    setcookie('raphbot_api_token', '', -1, dirname($_SERVER['PHP_SELF'])); 
     session_destroy();
     header('Location: login.php');
     exit();
